cb02eea5611ccff6d3de4e5c28ddb9ecb387f8f4
Bob Belderbos <bobbelderbos@gmail.com>
2019-02-19 00:18:43 +0100

    tweaks

--- packt.py
+++ packt.py
@@ -4,17 +4,24 @@ import json
 import os
 import re
 
-from selenium import webdriver
 import requests
-import tweepy
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import tweepy
 
 
+# selenium on heroku
 GOOGLE_CHROME_BIN = os.environ['GOOGLE_CHROME_BIN']
 CHROME_DRIVER = os.environ['CHROME_DRIVER']
+# twitter
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
+# slack
+SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
 
 PACKT_FREE_LEARNING = "https://www.packtpub.com/packt/offers/free-learning"
-HELP_TEXT = 'Packt free book (video) of the day'
 UPDATE_MSG = """Packt Free Learning of the day:
 {title} by {author} (published: {pub_date})
 
@@ -23,11 +30,6 @@ UPDATE_MSG = """Packt Free Learning of the day:
 Expires in {expires} ... grab it now!
 """
 
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
-SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
 
 Book = namedtuple('Book', 'title author pub_date expires')
 
@@ -40,32 +42,37 @@ def _create_update(book):
                              expires=book.expires)
 
 
-def get_packt_book():
+def _get_options():
     options = Options()
     options.add_argument("--headless")
     options.binary_location = GOOGLE_CHROME_BIN
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
+    return options
 
+
+def _get_expired(timer):
+    hh_mm_ss = re.sub(r'.*(\d{2}:\d{2}:\d{2}).*', r'\1', timer)
+    hours = hh_mm_ss.split(':')[0]
+    return f'in {int(hours)} hours'
+
+
+def get_packt_book():
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
-                              options=options)
+                              options=_get_options())
     driver.get(PACKT_FREE_LEARNING)
 
     find_class = driver.find_element_by_class_name
+
     title = find_class('product__title').text
     author = find_class('product__author').text
     pub_date = find_class('product__publication-date').text
-
-    timer_span = find_class('countdown__title').text.splitlines()[-1]
-    timer = re.sub(r'.*(\d{2}:\d{2}:\d{2}).*', r'\1', timer_span)
-    hours = timer.split(':')[0]
-    expires = f'in {int(hours)} hours'
+    timer = find_class('countdown__title').text.splitlines()[-1]
 
     driver.quit()
 
-    book = Book(title, author, pub_date, expires)
-    update = _create_update(book)
-    return update
+    book = Book(title, author, pub_date, _get_expired(timer))
+    return _create_update(book)
 
 
 def twitter_authenticate():
@@ -80,7 +87,7 @@ def post_to_twitter(book_post):
         api.update_status(book_post)
         print(f'Shared title on Twitter')
     except Exception as exc:
-        print(f'Error posting to Twitter - exception: {exc}')
+        print(f'Error posting to Twitter: {exc}')
 
 
 def post_to_slack(book_post):
@@ -92,14 +99,13 @@ def post_to_slack(book_post):
     if resp.status_code == 200:
         print(f'Shared title on Slack')
     else:
-        error = ('POST to Slack returned status code '
-                 f'{resp.status_code} and response '
-                 f'text: {resp.text}')
-        raise ValueError(error)
+        print(f'Error posting to Slack: {resp.status_code}')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=HELP_TEXT)
+    description = 'Packt free book (video) of the day'
+
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-t', '--twitter', action='store_true',
                         help="Post title to Twitter")
     parser.add_argument('-s', '--slack', action='store_true',
